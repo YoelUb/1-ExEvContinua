77eb2466fecf6b775c02d795fa76a7f57a1f0351
YoelUb <yurqubar@myuax.com>
2025-02-25 17:06:43 +0100

    Acabada Actividad 1, me dara tiempo?
    Rezando para que me de tiempo Ruben... ;)

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+package src.main.java;
+
+import src.main.java.actividad1.Matematicas;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------");
+        System.out.println("Actividad 1: ");
+        System.out.println("El numero Pi es: " + Matematicas.generarNumeroPi(Integer.parseInt(args[0])));
+        System.out.println("---------------------------------------------");
+
+
+        System.out.println("---------------------------------------------");
+        System.out.println("FUNCIONA ;)");
+        System.out.println(";) ----> 10000 de aura");
+        System.out.println(":) ------> -10000 de aura");
+
+    }
+}
---
+++ src/main/java/actividad1/Matematicas.java
@@ -0,0 +1,30 @@
+package src.main.java.actividad1;
+
+import java.util.Random;
+
+public class Matematicas {
+
+
+    public static double generarNumeroPi(long pasos) {
+        int areaCuadrado = 4;
+        int aciertos = 0;
+        Random rand = new Random();
+
+
+        for (int i = 0; i < pasos; i++){
+
+
+            double x = rand.nextDouble();
+            double y = rand.nextDouble();
+
+            if ((x * x) + (y * y) <= 1){
+                aciertos++;
+            }
+
+        }
+
+        return areaCuadrado * (aciertos / (double) pasos);
+
+    }
+
+}
