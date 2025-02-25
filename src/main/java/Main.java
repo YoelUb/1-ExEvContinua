f4ea1e02c3d518ed42ed0dd5bdb851b0b1302285
YoelUb <yurqubar@myuax.com>
2025-02-25 21:00:57 +0100

    Tablero acabado

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,15 @@ package src.main.java;
 
 import src.main.java.actividad1.Matematicas;
 
+
 public class Main {
+
+
     public static void main(String[] args) {
         System.out.println("---------------------------------------------");
         System.out.println("Actividad 1: ");
         System.out.println("El numero Pi es: " + Matematicas.generarNumeroPi(Integer.parseInt(args[0])));
         System.out.println("---------------------------------------------");
-
 
         System.out.println("---------------------------------------------");
         System.out.println("FUNCIONA ;)");
---
+++ src/main/java/actividad2/Principal.java
@@ -0,0 +1,4 @@
+package src.main.java.actividad2;
+
+public class Principal {
+}
