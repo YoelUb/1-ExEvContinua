4c787d7466dd7529db68e65316e1ade78754cc4e
YoelUb <yurqubar@myuax.com>
2025-02-25 21:13:28 +0100

    Actividad 2 acabada
    Compila y funciona, pero sera sufiente... ya veremos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 package src.main.java;
 
 import src.main.java.actividad1.Matematicas;
+import src.main.java.actividad2.Tablero;
+import java.util.concurrent.TimeUnit;
 
 
 public class Main {
@@ -11,6 +13,32 @@ public class Main {
         System.out.println("Actividad 1: ");
         System.out.println("El numero Pi es: " + Matematicas.generarNumeroPi(Integer.parseInt(args[0])));
         System.out.println("---------------------------------------------");
+        System.out.println("Actividad 2: ");
+        Tablero tablero = new Tablero();
+        try {
+            System.out.println("SIMULACIÓN CON TABLERO LEÍDO");
+            tablero.leerEstadoDesdeArchivo("docs/matriz.txt");
+            System.out.println(tablero);
+
+            for (int i = 0; i <= 5; i++) {
+                TimeUnit.SECONDS.sleep(1);
+                tablero.transitarAlEstadoSiguiente();
+                System.out.println(tablero);
+            }
+
+            System.out.println("SIMULACIÓN CON TABLERO GENERADO MEDIANTE MONTECARLO");
+            tablero.generarEstadoActualPorMontecarlo();
+            System.out.println(tablero);
+
+
+            for (int i = 0; i <= 15; i++) {
+                TimeUnit.SECONDS.sleep(1);
+                tablero.transitarAlEstadoSiguiente();
+                System.out.println(tablero);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Error durante la simulación: " + e);
+        }
 
         System.out.println("---------------------------------------------");
         System.out.println("FUNCIONA ;)");
--- src/main/java/actividad2/Principal.java
+++
@@ -1,4 +0,0 @@
-package src.main.java.actividad2;
-
-public class Principal {
-}
